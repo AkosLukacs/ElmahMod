290e44ea6d5c01fa4c92ed4861b9a1c38e31ec55
azizatif <azizatif@7aa70103-2b1f-0410-8bfa-59e2bdee2fba>
2007-04-26 23:26:38 +0000

    * Deleted the old license RTF and replaced it with the New BSD License at the root. * Updated the header at the start of all source files to reflect the New BSD License. * Updated copyright notice in AssemblyCopyright attribute.

--- src/Elmah/AssemblyInfo.cs
+++ src/Elmah/AssemblyInfo.cs
@@ -1,17 +1,28 @@
-#region Byline & Disclaimer
+#region License, Terms and Author(s)
+//
+// ELMAH - Error Logging Modules and Handlers for ASP.NET
+// Copyright (c) 2007 Atif Aziz. All rights reserved.
 //
 //  Author(s):
 //
-//      Atif Aziz (atif.aziz@skybow.com, http://www.raboof.com)
+//      Atif Aziz, http://www.raboof.com
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the New BSD License, a copy of which should have 
+// been delivered along with this distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 #endregion
 
 #region Imports
@@ -27,7 +38,7 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 [assembly: AssemblyDescription("Error Logging Modules and Handlers (ELMAH) for ASP.NET")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ELMAH")]
-[assembly: AssemblyCopyright("Copyright (c) 2004, Atif Aziz, Skybow AG. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2007, Atif Aziz. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("1.0.5527.0")]
