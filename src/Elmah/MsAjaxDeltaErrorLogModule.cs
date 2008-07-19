cefdc7fdde84915a362129bc69ea6b59e1cb6556
jamesdriscoll@btinternet.com <jamesdriscoll@btinternet.com@7aa70103-2b1f-0410-8bfa-59e2bdee2fba>
2008-07-19 09:59:17 +0000

    * Updated the samples/web.config to include the MsAjaxDeltaErrorLogModule * Added remarks to MsAjaxDeltaErrorLogModule to describe its usage, noting that it is not necessary for the version of MS Ajax shipped with .Net Framework v3.5

--- src/Elmah/MsAjaxDeltaErrorLogModule.cs
+++ src/Elmah/MsAjaxDeltaErrorLogModule.cs
@@ -46,6 +46,29 @@ namespace Elmah
     /// request issued by the client when a page uses the UpdatePanel
     /// introduced with ASP.NET 2.0 AJAX Extensions.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This module is ONLY required when dealing with v1.0.x.x of System.Web.Extensions.dll
+    /// (i.e. the downloadable version to extend v2.0 of the .Net Framework)
+    /// </para>
+    /// <para>
+    /// Using it with v3.5 of System.Web.Extensions.dll (which shipped as part of v3.5 of the 
+    /// .Net Framework) will result in a duplication of errors.
+    /// </para>
+    /// <para>
+    /// This is because MS have changed the implementation of 
+    /// System.Web.UI.PageRequestManager.OnPageError
+    /// </para>
+    /// <para>
+    /// In v1.0.x.x, the code performs a brutal <code>Response.End();</code> in an attempt to
+    /// "tidy up"! This means that the error will not bubble up to the Application.Error 
+    /// handlers, so Elmah is unable to catch them.
+    /// </para>
+    /// <para>
+    /// In v3.5, this is handled much more gracefully, allowing Elmah to do its thing without
+    /// the need for this module!
+    /// </para>
+    /// </remarks>
 
     public class MsAjaxDeltaErrorLogModule : IHttpModule
     {
