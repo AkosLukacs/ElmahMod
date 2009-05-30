30ac4cbd85a6b1b489a546c40ec17537bf0eba5c
azizatif <azizatif@7aa70103-2b1f-0410-8bfa-59e2bdee2fba>
2009-05-30 01:57:18 +0000

    Added extension methods for Dictionary.

---
+++ src/Elmah/DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Elmah
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+
+    #endregion
+
+    static class DictionaryExtensions
+    {
+        public static V Find<K, V>(this IDictionary<K, V> dict, K key)
+        {
+            return Find(dict, key, default(V));
+        }
+
+        public static V Find<K, V>(this IDictionary<K, V> dict, K key, V @default)
+        {
+            if (dict == null) throw new ArgumentNullException("dict");
+            V value;
+            return dict.TryGetValue(key, out value) ? value : @default;
+        }
+    }
+}
